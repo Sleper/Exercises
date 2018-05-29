e4073b460ac2aa146914f9727678bf676688c1c8
Sleper <saliamonasalgirdas@gmail.com>
2018-05-29 19:14:03 +0300

    Update 5. Array_Sum.js

--- 5. Array_Sum.js
+++ 5. Array_Sum.js
@@ -1,3 +1,4 @@
+//Write JS program to compute the sum of an array of integers (example: [1,2,3,4,5,6,7] => expected output: 28)
 function sumArray(arr){
 	var sum = 0;
 	for (var i = 0; i < arr.length; i++){
