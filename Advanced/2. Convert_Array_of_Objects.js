4bf31d00bdb193b7d459bf5c8ca3a1a72c90fbca
Sleper <saliamonasalgirdas@gmail.com>
2018-05-31 04:31:12 +0300

    Another attempt at the proper output

--- Advanced/2. Convert_Array_of_Objects.js
+++ Advanced/2. Convert_Array_of_Objects.js
@@ -21,20 +21,43 @@ var pets = [
 // }
 
 
-function covertArray(arr){
+function convertArray(arr){
 	var newPets = {};
-	for ( var i = 0; i < arr.length; i++){
-		if (arr[i].parentId ==  null){
-			for( var j = 0; j < arr.length; j++){
-				console.log(arr[j].name)
-				if(arr[j].parentId == arr[0].id){
-					newPets[arr[0].name] = arr[j].name;
-				} 
-				if(arr[j].parentId == arr[3].id){
-					newPets[arr[3].name] == arr[j].name;
-				}
-			}
+	var dogs = [];
+	var cats = [];
+	var j = 0;
+	var k = 0;
+	for (var i = 0; i < arr.length; i++){
+		if(arr[i].parentId == arr[0].id) {
+			dogs[j] = arr[i].name;
+			j++;
+		} 
+		if(arr[i].parentId == arr[3].id){
+			cats[k] = arr[i].name;
+			k++;
 		}
 	}
+	newPets[arr[0].name] = dogs;
+	newPets[arr[3].name] = cats;
 	return newPets;
 }
+
+
+// function covertArray(arr){
+// 	var newPets = {};
+// 	for ( var i = 0; i < arr.length; i++){
+// 		if (arr[i].parentId ==  null){
+// 			for( var j = 0; j < arr.length; j++){
+// 				if(arr[j].parentId == arr[0].id){
+// 					newPets[arr[0].name] = arr[j].name;
+// 				} 
+// 				if(arr[j].parentId == arr[3].id){
+// 					newPets[arr[3].name] == arr[j].name;
+// 				}
+// 			}
+// 		}
+// 	}
+// 	return newPets;
+// }
+
+
